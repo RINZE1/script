d75f7e198a870c58a5d2aecf2c38a8af45708292
Breakdown <169538186+RINZE1@users.noreply.github.com>
2025-11-11 12:58:07 +0800

    Modify HRV settings in ecgpro_settings_modifier.js

--- ecgpro_settings_modifier.js
+++ ecgpro_settings_modifier.js
@@ -1,12 +1,13 @@
 if ($request.url.endsWith('settings-hrv.txt')) {
     let body = JSON.parse($response.body);
-
     if (body.settings) {
         body.settings["hrv-enable-pro-purchase"] = true;
         body.settings["enable-asking-question"] = true;
         body.settings["chatgpt-daily-limit"] = 114514;
+        body.settings["hrv-free-count"] = 114514;
+        body.settings["more-count-after-rate"] = 114514;
+        body.settings["rate-get-more"] = true;
     }
-
     $done({body: JSON.stringify(body)});
 } else {
     $done({});
